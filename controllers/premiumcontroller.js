7f91cfde752883f8998e1b628b12b5ef8b74fa7a
mohitr Agarwal <bmohit700@gmail.com>
2023-06-10 12:45:55 +0530

    implementing download button

--- controllers/premiumcontroller.js
+++ controllers/premiumcontroller.js
@@ -1,20 +1,54 @@
 const sequelize = require("../database");
 const Expenses = require("../models/expenses");
 const USERS = require("../models/user");
-
+const AWS = require("aws-sdk");
+require("dotenv").config();
 async function showleaderboard(req, res, next) {
   try {
-
     const user = await USERS.findAll({
-        attributes:['name','total_exp'],
-        order:[['total_exp','DESC']]
+      attributes: ["name", "total_exp"],
+      order: [["total_exp", "DESC"]],
     });
 
-    res.status(200).json({user})
-
+    res.status(200).json({ user });
   } catch (err) {
     console.log(err);
   }
 }
+async function downloadrep(req, res) {
+  const exp = await req.user.getExpenses();
+  const stringexp = JSON.stringify(exp);
+  const userId=req.user.id
+  const filename = 'Expense'+userId+'/'+new Date()+'.txt';
+  const fileURl = await uploadToS3(stringexp, filename);
+  res.status(200).json({ fileURl, success: true });
+}
+function uploadToS3(data, filename) {
+  const BUCKET_NAME = process.env.BUCKET_NAME;
+  const IAM_USER_KEY = process.env.IAM_USER_KEY;
+  const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
 
-module.exports = { showleaderboard };
+  let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET,
+  });
+    var params = {
+      Bucket: BUCKET_NAME,
+      Key: filename,
+      Body: data,
+      ACL:"public-read"
+    };
+    return new Promise((resolve,reject)=>{
+      s3bucket.upload(params,(err,response)=>{
+        if(err){
+          console.log(err);
+          reject(err)
+        }else{
+          resolve(response.Location)
+        }
+      })
+    })
+
+}
+
+module.exports = { showleaderboard, downloadrep };
--- routes/premium.js
+++ routes/premium.js
@@ -5,5 +5,7 @@ const userAuthentication=require('../middleware/authorization')
 const router=express.Router();
 
 router.get('/premium/showleaderboard',userAuthentication.authenticate,premiumcontroller.showleaderboard);
+router.get('/premium/downrep',userAuthentication.authenticate,premiumcontroller.downloadrep);
+
 
 module.exports=router;
